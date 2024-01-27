3852fc1b683c7a02b35ea0800d50a695c89e24aa
padmakar <rakeshahirrao29@gmail.com>
2024-01-27 09:38:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import './styles.css';
+import ProgressBar from "./components/ProgressBar";
+
+function App() {
+
+  const [value,setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(()=> {
+      setInterval(() => {
+        setValue((val) => val + 0.1);
+      }, 20);
+  },[]);
+
+  return (
+    <div className="app">
+        <span>Progress Bar</span>
+        <ProgressBar value={value} onComplete={() => setSuccess(true)}></ProgressBar>
+        <span>{success? "Completed" : "Loading..."}</span>
+    </div>
+  );
+}
+
+export default App;
